ee752e41cf38d3c7e49cdd0f42fda4e0b012841b
David Craven <david@craven.ch>
2020-11-07 17:11:19 +0100

    Improve derive macro.

--- derive/examples/module_test.rs
+++ derive/examples/module_test.rs
@@ -1,6 +1,6 @@
-use hlua_derive::lua_module;
+use hlua_derive::lua;
 
-#[lua_module]
+#[lua]
 pub mod mylib {
     pub static PI: f32 = 3.141592;
 
@@ -11,18 +11,115 @@ pub mod mylib {
     fn function2(a: u32) -> u32 {
         a + 5
     }
+}
 
-    #[init]
-    fn init() {
-        println!("mylib is now loaded!")
+#[lua]
+pub mod vec3 {
+    #[derive(Clone, Copy, Debug, PartialEq)]
+    pub struct Vec3(f64, f64, f64);
+
+    impl Vec3 {
+        pub fn new(x: f64, y: f64, z: f64) -> Self {
+            Self(x, y, z)
+        }
+
+        #[lua(meta = "__add")]
+        pub fn add(&self, other: &Vec3) -> Self {
+            Self(self.0 + other.0, self.1 + other.1, self.2 + other.2)
+        }
+
+        #[lua(meta = "__tostring")]
+        pub fn to_string(&self) -> String {
+            format!("[ {}, {}, {} ]", self.0, self.1, self.2)
+        }
+    }
+}
+
+pub struct LuaSandbox<'lua> {
+    lua: hlua::Lua<'lua>,
+}
+
+impl<'lua> LuaSandbox<'lua> {
+    const USER_FUNCTION_NAME: &'static str = "__user_function__";
+    const SANDBOX_ENV_NAME: &'static str = "__sandbox_env__";
+    // This env is taken from:
+    // http://stackoverflow.com/questions/1224708/how-can-i-create-a-secure-lua-sandbox
+    const SANDBOX_ENV: &'static str = "{
+        ipairs = ipairs,
+        next = next,
+        pairs = pairs,
+        pcall = pcall,
+        print = print,
+        tonumber = tonumber,
+        tostring = tostring,
+        type = type,
+        unpack = unpack,
+        coroutine = { create = coroutine.create, resume = coroutine.resume,
+            running = coroutine.running, status = coroutine.status,
+            wrap = coroutine.wrap },
+        string = { byte = string.byte, char = string.char, find = string.find,
+            format = string.format, gmatch = string.gmatch, gsub = string.gsub,
+            len = string.len, lower = string.lower, match = string.match,
+            rep = string.rep, reverse = string.reverse, sub = string.sub,
+            upper = string.upper },
+        table = { insert = table.insert, maxn = table.maxn, remove = table.remove,
+            sort = table.sort },
+        math = { abs = math.abs, acos = math.acos, asin = math.asin,
+            atan = math.atan, atan2 = math.atan2, ceil = math.ceil, cos = math.cos,
+            cosh = math.cosh, deg = math.deg, exp = math.exp, floor = math.floor,
+            fmod = math.fmod, frexp = math.frexp, huge = math.huge,
+            ldexp = math.ldexp, log = math.log, log10 = math.log10, max = math.max,
+            min = math.min, modf = math.modf, pi = math.pi, pow = math.pow,
+            rad = math.rad, random = math.random, sin = math.sin, sinh = math.sinh,
+            sqrt = math.sqrt, tan = math.tan, tanh = math.tanh },
+        os = { clock = os.clock, difftime = os.difftime, time = os.time },
+    }";
+
+    pub fn new() -> Result<Self, hlua::LuaError> {
+        let mut lua = hlua::Lua::new();
+        lua.openlibs();
+        lua.execute(&format!(
+            "{env} = {val}",
+            env = Self::SANDBOX_ENV_NAME,
+            val = Self::SANDBOX_ENV,
+        ))?;
+        Ok(Self { lua })
+    }
+
+    pub fn load<'a, F>(&'a mut self, luaopen: &F)
+    where
+        F: Fn(hlua::LuaTable<hlua::PushGuard<&'a mut hlua::Lua<'lua>>>),
+    {
+        let env = self.lua.get(Self::SANDBOX_ENV_NAME).unwrap();
+        luaopen(env);
+    }
+
+    pub fn execute<'a, T>(&'a mut self, script: &str) -> Result<T, hlua::LuaError>
+    where
+        T: for<'g> hlua::LuaRead<hlua::PushGuard<&'g mut hlua::PushGuard<&'a mut hlua::Lua<'lua>>>>,
+    {
+        self.lua
+            .checked_set(Self::USER_FUNCTION_NAME, hlua::LuaCode(script))?;
+        self.lua.execute(&format!(
+            "debug.setupvalue({f}, 1, {env}); return {}();",
+            f = Self::USER_FUNCTION_NAME,
+            env = Self::SANDBOX_ENV_NAME,
+        ))
     }
 }
 
 fn main() {
-    let mut lua = hlua::Lua::new();
-    mylib::luaopen(&mut lua);
+    let mut lua = LuaSandbox::new().unwrap();
+    lua.load(&mylib::load);
     let res: u32 = lua.execute("return mylib.function1(3, 4)").unwrap();
     assert_eq!(res, 7);
     let res: f32 = lua.execute("return mylib.PI").unwrap();
     assert_eq!(res, mylib::PI);
+
+    lua.load(&vec3::load);
+    let res: vec3::Vec3 = lua
+        .execute("return vec3.new(1.0, 2.0, 3.0) + vec3.new(3.0, 2.0, 1.0)")
+        .unwrap();
+    assert_eq!(res, vec3::Vec3::new(4.0, 4.0, 4.0));
+    println!("result is {}", res.to_string());
 }
--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -1,87 +1,264 @@
 extern crate proc_macro;
 
-use proc_macro::TokenStream;
-use proc_macro_error::{abort_call_site, emit_warning, proc_macro_error};
+use proc_macro2::TokenStream;
+use proc_macro_error::{abort_call_site, proc_macro_error};
 use quote::{format_ident, quote};
+use std::collections::HashMap;
 
-#[proc_macro_attribute]
-#[proc_macro_error]
-pub fn lua_module(_args: TokenStream, input: TokenStream) -> TokenStream {
-    let module: syn::ItemMod = syn::parse(input).unwrap_or_else(|_| {
-        abort_call_site!("`export_lua_module` attribute is only allowed on modules");
-    });
+#[derive(Clone, Debug)]
+enum Value {
+    Table(Table),
+    Value(TokenStream),
+}
 
-    if let syn::Visibility::Public(_) = &module.vis {
-    } else {
-        abort_call_site!("`export_lua_module` attribute is only allowed on public modules");
+#[derive(Clone, Debug, Default)]
+struct Table {
+    entries: HashMap<String, Value>,
+}
+
+impl Table {
+    fn new() -> Self {
+        Self::default()
     }
 
-    let mut table = Vec::new();
-    let mut mod_items = Vec::new();
-    let mut init = Vec::new();
+    fn add_table(&mut self, name: String, table: Table) {
+        self.entries.insert(name, Value::Table(table));
+    }
 
-    if let Some((_, items)) = module.content.as_ref() {
-        for item in items {
-            match item {
-                syn::Item::Fn(f) => {
-                    let fn_ident = &f.sig.ident;
-                    let fn_name = fn_ident.to_string();
-                    let n_args = f.sig.inputs.len();
-                    let wrapper = format_ident!("function{}", n_args);
-                    table.push(quote! {
-                        table.set(#fn_name, hlua::#wrapper(#fn_ident));
-                    });
+    fn add_static(&mut self, item: &syn::ItemStatic) {
+        let ident = &item.ident;
+        let name = ident.to_string();
+        self.entries.insert(name, Value::Value(quote!(#ident)));
+    }
 
-                    let mut new_f = f.clone();
-                    new_f.attrs = f
-                        .attrs
-                        .iter()
-                        .filter(|attr| {
-                            if let Some(ident) = attr.path.get_ident() {
-                                if ident.to_string() == "init" {
-                                    return false;
-                                }
+    fn add_fn(&mut self, item: &syn::ItemFn) {
+        let ident = &item.sig.ident;
+        let name = ident.to_string();
+        let n_args = item.sig.inputs.len();
+        self.insert(name, n_args, quote!(#ident));
+    }
+
+    fn insert(&mut self, name: String, n_args: usize, tokens: TokenStream) {
+        let wrapper = format_ident!("function{}", n_args);
+        self.entries
+            .insert(name, Value::Value(quote!(hlua::#wrapper(#tokens))));
+    }
+
+    fn into_tokens(self, ident: &syn::Ident) -> TokenStream {
+        self.entries
+            .into_iter()
+            .map(|(k, v)| match v {
+                Value::Table(table) => {
+                    if table.entries.is_empty() {
+                        quote!()
+                    } else {
+                        let child = format_ident!("{}", k);
+                        let tokens = table.into_tokens(&child);
+                        quote! {
+                            {
+                                let mut #child = #ident.empty_array(#k);
+                                #tokens
                             }
-                            true
-                        })
-                        .cloned()
-                        .collect();
-                    mod_items.push(quote!(#new_f));
-
-                    if new_f.attrs.len() != f.attrs.len() {
-                        init.push(quote!(#fn_ident();));
+                        }
                     }
                 }
-                syn::Item::Static(s) => {
-                    let ident = &s.ident;
-                    let name = ident.to_string();
-                    table.push(quote! {
-                        table.set(#name, #ident);
-                    });
-                    mod_items.push(quote!(#s));
-                }
-                _ => {
-                    emit_warning!(
-                        "item is neiter a function nor a static and will ",
-                        "thus be ignored by `export_lua_module`",
-                    );
-                }
-            }
+                Value::Value(tokens) => quote!(#ident.set(#k, #tokens);),
+            })
+            .collect()
+    }
+
+    fn table_mut(&mut self, name: String) -> &mut Table {
+        let value = self
+            .entries
+            .entry(name)
+            .or_insert_with(|| Value::Table(Table::new()));
+        if let Value::Table(ref mut table) = value {
+            table
+        } else {
+            panic!("internal error")
+        }
+    }
+}
+
+struct Object {
+    self_ty: Box<syn::Type>,
+    constructors: Table,
+    metatable: Table,
+}
+
+impl Object {
+    fn new(self_ty: Box<syn::Type>) -> Self {
+        Self {
+            self_ty,
+            constructors: Table::new(),
+            metatable: Table::new(),
         }
     }
 
-    let ident = &module.ident;
-    let name = ident.to_string();
+    fn add_method(&mut self, item: &syn::ImplItemMethod, meta: Option<String>) {
+        let ty = &self.self_ty;
+        let ident = &item.sig.ident;
+        let name = ident.to_string();
+        let n_args = item.sig.inputs.len();
+        match &item.sig.inputs[0] {
+            syn::FnArg::Receiver(_) => {
+                let args_decl = item.sig.inputs.iter().skip(1).collect::<Vec<_>>();
+                let args = args_decl.iter().map(|arg| {
+                    if let syn::FnArg::Typed(pat) = arg {
+                        &pat.pat
+                    } else {
+                        unreachable!()
+                    }
+                });
+                let f = quote!(|o: &mut #ty, #(#args_decl),*| o.#ident(#(#args),*));
+                if let Some(meta) = meta {
+                    self.metatable.insert(meta, n_args, f)
+                } else {
+                    let index = self.metatable.table_mut("__index".to_string());
+                    index.insert(name, n_args, f);
+                }
+            }
+            _ => {
+                let f = quote!(#ty::#ident);
+                self.constructors.insert(name, n_args, f);
+            }
+        };
+    }
+}
+
+#[derive(Default)]
+struct Module {
+    ident: String,
+    objects: Vec<Object>,
+    table: Table,
+}
+
+impl Module {
+    fn new(ident: String) -> Self {
+        Self {
+            ident,
+            objects: Vec::new(),
+            table: Table::new(),
+        }
+    }
+
+    fn add_static(&mut self, item: &syn::ItemStatic) {
+        self.table.add_static(item);
+    }
+
+    fn add_fn(&mut self, item: &syn::ItemFn) {
+        self.table.add_fn(item);
+    }
+
+    fn add_object(&mut self, obj: Object) {
+        self.objects.push(obj);
+    }
+
+    fn into_tokens(mut self) -> TokenStream {
+        let objects: TokenStream = self
+            .objects
+            .iter()
+            .map(|obj| {
+                let ident = &obj.self_ty;
+                let tokens = obj
+                    .metatable
+                    .clone()
+                    .into_tokens(&format_ident!("metatable"));
+                quote! {
+                    hlua::implement_lua_push!(#ident, |mut metatable| {
+                        #tokens
+                    });
+                    hlua::implement_lua_read!(#ident);
+                }
+            })
+            .collect();
+        for obj in self.objects {
+            for (name, cons) in obj.constructors.entries {
+                self.table.entries.insert(name, cons);
+            }
+        }
+        let mut module = Table::new();
+        module.add_table(self.ident, self.table);
+        let module = module.into_tokens(&format_ident!("lua"));
+        quote! {
+            #objects
+
+            pub fn load<'a, L: hlua::AsMutLua<'a>>(mut lua: hlua::LuaTable<L>) {
+                #module
+            }
+        }
+    }
+}
+
+#[proc_macro_attribute]
+#[proc_macro_error]
+pub fn lua(
+    _args: proc_macro::TokenStream,
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let mut item_mod: syn::ItemMod = syn::parse(input).unwrap_or_else(|_| {
+        abort_call_site!("`lua_module` attribute is only allowed on modules");
+    });
+
+    if let syn::Visibility::Public(_) = &item_mod.vis {
+    } else {
+        abort_call_site!("`lua_module` attribute is only allowed on public modules");
+    }
+
+    let mut module = Module::new(item_mod.ident.to_string());
+    let mut mod_items = Vec::new();
+
+    if let Some((_, items)) = item_mod.content.as_mut() {
+        for item in items {
+            match item {
+                syn::Item::Fn(f) => module.add_fn(f),
+                syn::Item::Static(s) => module.add_static(s),
+                syn::Item::Impl(i) => {
+                    let mut obj = Object::new(i.self_ty.clone());
+                    for item in &mut i.items {
+                        if let syn::ImplItem::Method(item) = item {
+                            let mut new_attrs = Vec::with_capacity(item.attrs.len());
+                            let mut metatable = None;
+                            for attr in &item.attrs {
+                                if attr.path.is_ident("lua") {
+                                    if let Ok(syn::Meta::List(meta)) = attr.parse_meta() {
+                                        if let Some(syn::NestedMeta::Meta(syn::Meta::NameValue(
+                                            nv,
+                                        ))) = meta.nested.first()
+                                        {
+                                            if nv.path.is_ident("meta") {
+                                                if let syn::Lit::Str(value) = &nv.lit {
+                                                    metatable = Some(value.value());
+                                                    continue;
+                                                }
+                                            }
+                                        }
+                                    }
+                                    abort_call_site!("unrecognized attribute");
+                                } else {
+                                    new_attrs.push(attr.clone());
+                                }
+                            }
+                            item.attrs = new_attrs;
+                            obj.add_method(&item, metatable);
+                        }
+                    }
+                    module.add_object(obj);
+                }
+                _ => {}
+            };
+            mod_items.push(item.clone());
+        }
+    }
+
+    let ident = &item_mod.ident;
+    let lua_module = module.into_tokens();
 
     (quote! {
         pub mod #ident {
             #(#mod_items)*
 
-            pub fn luaopen(lua: &mut hlua::Lua) {
-                let mut table = lua.empty_array(#name);
-                #(#table)*
-                #(#init)*
-            }
+            #lua_module
         }
     })
     .into()
